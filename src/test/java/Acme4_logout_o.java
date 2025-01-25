3febcd07f6edbb83211789d048d3dd6d1024e16b
unknown <dipesh.pandey42@gmail.com>
2025-01-25 11:09:36 +0545

    Logout test cases added

--- src/test/java/Acme4_logout_o.java
+++ src/test/java/Acme4_logout_o.java
@@ -4,25 +4,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class Ame2_LoginUser_o {
+public class Acme4_logout_o {
     WebDriver driver;
+
     @BeforeTest
-    public void OpenBrowser(){
+    public void Setup(){
         System.setProperty("webdriver.chrome.driver","src/test/ChromeDriver/chromedriver.exe");
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         driver.get("https://automationexercise.com/");
-    }
-    @Test
-    public void CheckLogin(){
-        Login l = new Login(driver);
 
+    }
+
+    @Test
+    public void Logout(){
+        Login l = new Login(driver);
+        l.verifyHome();
         l.clickLogin();
         l.verifyLoginPage();
         l.Login("dipesh.pandey42@gmail.com","dipesh");
+        l.verifyLogin();
+        l.clickLogout();
+        l.verifyLoginPage();
+
     }
-
-
 }
--- src/test/java/Page_Objects/Login.java
+++ src/test/java/Page_Objects/Login.java
@@ -12,22 +12,33 @@ public class Login {
     public Login(WebDriver driver){
         this.driver = driver;
     }
+
+    public void verifyHome(){
+        WebElement home = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[1]/a"));
+        Assert.assertEquals(home.getText(),"Home");
+    }
     public void clickLogin(){
         driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a")).click();
     }
-    public void verifyWelcome(){
+    public void verifyLoginPage(){
         WebElement welcome = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/h2"));
         Assert.assertEquals(welcome.getText(),"Login to your account");
     }
-    public void checkLogin(String username, String password){
+    public void Login(String username, String password){
         WebElement u = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[2]"));
         u.sendKeys(username);
         WebElement p = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[3]"));
         p.sendKeys(password);
         driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/button")).click();
 
+    }
+    public void verifyLogin(){
         WebElement verify = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[10]/a"));
         Assert.assertEquals(verify.getText(),"Logged in as dipesh");
     }
+    public void clickLogout(){
+        driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a")).click();
+    }
+
 
 }
